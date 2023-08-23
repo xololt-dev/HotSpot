b457ed7b2c3fe82d46aaf4a901b8f5521528aff9
Xololt <105434979+xololt-dev@users.noreply.github.com>
2023-08-24 00:58:03 +0200

    hitLocation prototype enum added WorldObject destructor fixed StaticControlsObject class added GameObject QOL constructor change & destructor added Helper functions added

--- game/src/objects.hpp
+++ game/src/objects.hpp
@@ -3,6 +3,13 @@
 #include <vector>
 #include <iterator>
 
+enum hitLocation {
+	LEFT,
+	RIGHT,
+	TOP,
+	BOTTOM
+};
+
 class GameObject;
 
 class WorldObject {
@@ -15,10 +22,16 @@ public:
 	std::vector<GraphicsObject* > graphicsObjectsArray;
 	*/
 	~WorldObject() {
+		std::vector<GameObject* >::iterator it;
+		for (it = gameObjectsArray.begin(); it != gameObjectsArray.end(); it++) {
+			delete *it;
+		}
 		gameObjectsArray.clear();
 	}
 
 	void initialize();
+	void update();
+	void draw();
 
 private:
 	
@@ -47,9 +60,30 @@ private:
 	float ACCELERATION = 1.0f;
 };
 
+class StaticControlsObject : public ControlsObject {
+public:
+	StaticControlsObject() {}
+
+	StaticControlsObject(float ACCELERATION_) :
+		ACCELERATION(ACCELERATION_)
+	{}
+
+	// enum for checking if controller/touchscreen/keyboard
+	void update(GameObject& gameObject) {};
+
+private:
+	float ACCELERATION = 0.0f;
+};
+
 class PhysicsObject {
 public:
+	PhysicsObject(Rectangle hitbox_) : 
+		hitbox(hitbox_) 
+	{}
+
 	void update(GameObject& gameObject, WorldObject& world);
+
+	Rectangle getHitbox() { return hitbox; };
 
 private:
 	Rectangle hitbox;
@@ -63,7 +97,7 @@ public:
 		physics(physics_)
 	{}
 
-	void update(GameObject& gameObject);
+	void update(GameObject& gameObject) {};
 
 private:
 	// Physics
@@ -82,7 +116,7 @@ public:
 
 	GameObject(PhysicsObject* physics_, GraphicsObject* graphics_, ControlsObject* controls_) :
 		physics(physics_), graphics(graphics_), controls(controls_) {
-		position = { 0.0f, 0.0f },
+		position = { physics->getHitbox().x, physics->getHitbox().y },
 		velocity = { 0.0f, 0.0f };
 	}
 
@@ -90,9 +124,15 @@ public:
 		physics(physics_), graphics(graphics_), controls(controls_), position(position_), velocity(velocity_)
 	{}
 
+	~GameObject() {
+		delete controls;
+		delete physics;
+		delete graphics;
+	}
+
 	void update(WorldObject& world);
 
-	PhysicsObject* getPhysicsObjects() { return physics; }
+	PhysicsObject* getPhysicsObject() { return physics; }
 
 private:
 	// Controls
